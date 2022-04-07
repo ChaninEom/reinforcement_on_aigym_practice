d3a0151f1a19516bef12fa76e3a29945edf7dc76
ChaninEom <eci0623@naver.com>
2022-04-08 03:25:59 +0900

    Fix : FrozenLake.py slipery 문제 해결
    지난 commit 때, action과 다른 방향으로 state 이동하는 문제 해결
    이는 env를 만들 때, slipery=True로 되어있기 때문이었음
    즉, 특정 확률로 미끄러져서 action과 다른 state로 이동(frozen 이여서 그런듯.....)
    해당 설정을 바꿔 준 뒤, hole에 빠졌을 때 해당 table을 -1로 갱신
    초기 eps값 1로 설정하여 random 100%로 설정한 뒤, 이후 goal에 다다를 때마다
    eps*0.99를 해줌으로써 policy 기반 움직임 강화
    최종 1663번 째 episode에서의 q-table은
    [[ 0.78231889  0.78285099  0.78294379  0.78284039]
     [ 0.78734354 -1.          0.7875522   0.7874086 ]
     [ 0.78963377  0.60517582  0.78986037  0.78975276]
     [ 0.79267462 -1.          0.68008213  0.64118495]
     [ 0.78600449  0.78620008 -1.          0.78604156]
     [-1.          0.5509212  -1.          0.70573085]
     [ 0.76101729 -1.          0.62683614  0.74262317]
     [ 0.49114198  0.26839569  0.74121256 -1.        ]
     [ 0.45214092  0.8828984  -1.          0.60746474]
     [ 0.90735451  0.59867509  0.39721736  0.57521113]
     [ 0.96238449  0.24059552  0.51053483  0.1419726 ]
     [-1.          0.48957644  0.70890309  0.50063293]
     [ 0.18220656  0.55525331  0.98651121  0.39641398]
     [ 0.64227265  0.22053893  0.25884257  0.10837349]]
    으로 아직 개선해야할 부분이 많지만 더 고민해볼 예정

--- Toy_text/FrozenLake.py
+++ Toy_text/FrozenLake.py
@@ -1,18 +1,26 @@
 import os, sys
-from sre_parse import State
 import gym
 import numpy as np
 import random
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from utils.GymAgent import Agent
 
+from gym.envs.registration import registry, register
+
+register(
+    id="FrozenLake-v1",
+    entry_point="gym.envs.toy_text:FrozenLakeEnv",
+    kwargs={"map_name": "4x4", "is_slippery": False},
+    max_episode_steps=100,
+    reward_threshold=0.70,  # optimum = 0.74
+)
 env_name = "FrozenLake-v1"
 env = gym.make(env_name)
 
 class QAgent(Agent):
     def __init__(self, env, discount_rate = 0.97, learning_rate = 0.01):
         super().__init__(env)
-        self.eps = 1.0
+        self.eps = 1
         self.state_size = env.observation_space.n
         self.discout_rate = discount_rate
         self.learning_rate = learning_rate
@@ -27,7 +35,14 @@ class QAgent(Agent):
         q_state = self.q_table[state]
         action_greedy = np.argmax(q_state)
         action_random = super().get_action(state)
-        return action_random if random.random() < self.eps else action_greedy
+        
+        rand_num = random.random()
+        if rand_num<self.eps:
+            print('random!')
+            return action_random
+        else:
+            return action_greedy
+        # return action_random if random.random() < self.eps else action_greedy
     
     def train(self, experience):
         state, action, next_state, reward, done = experience
@@ -36,27 +51,33 @@ class QAgent(Agent):
         q_next = np.zeros([self.action_size]) if done else q_next
         q_target = reward + self.discout_rate * np.max(q_next)
         q_update = q_target - self.q_table[state,action]
+        print(self.eps)
         self.q_table[state,action] += self.learning_rate * q_update
-        
-        if done:
+        if next_state in [5, 7, 11, 12]:
+            self.q_table[state, action] = -1
+        if next_state ==15:
+            print('==========done=================')
             self.eps = self.eps * 0.99
 
 agent = QAgent(env)
-
+print(env.__init__)
 total_reward = 0
-for ep in range(200):
+for ep in range(2000):
     state = env.reset()
     done = False
     print('rest')
     while not done:
         action = agent.get_action(state)
         next_state, reward, done, info = env.step(action)
-        import time
-        t=['right', 'left', 'up', 'down']
-        print("==================")
-        print(t[action])
-        print("==================")
-        time.sleep(2)
+
+        # import time
+        # t=['left', 'down', 'right', 'up']
+        # print("==================")
+        # print(t[action])
+        # print("==================")
+        # time.sleep(3)
+
+        print(agent.q_table)
         agent.train((state, action, next_state, reward, done))
         state = next_state
         total_reward += reward
--- test.py
+++ test.py
@@ -1,3 +1,2 @@
-import numpy as np
-a = [1, 7, 3, 4]
-print(np.argmax(a))
+import random
+print(random.random())
