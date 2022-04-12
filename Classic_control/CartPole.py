4fe9924ecc70d3f93e3c53bcc1079cc2da1a714a
ChaninEom <eci0623@naver.com>
2022-04-13 01:34:06 +0900

    Fix : CartPole.py 구성 변경
    기존 random 하게 action 받아오던 cartpole을 poleagent class 생성하여
    적절한 action 값 받아오도록 갱신 weight matrix와 bias term을 사용하여
    각 step에서의 reward 값을 기준으로 갱신
    
    but, aigym docs의 solved requirements는 아직 충족하지 못함
    195보다 reward 커야하면 100 episode 연속으로 해당 reward가 반복되야함
    더 수정해 볼 예정

--- Classic_control/CartPole.py
+++ Classic_control/CartPole.py
@@ -1,5 +1,7 @@
+from math import gamma
 import os, sys
 import gym
+import numpy as np
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from utils.GymAgent import Agent
@@ -7,12 +9,45 @@ from utils.GymAgent import Agent
 env_name = 'CartPole-v1'
 env = gym.make(env_name)
 
-# print(env.observation_space)
-# print(env.action_space)
+class PoleAgent(Agent):
+    def __init__(self, env, gamma = 1e-2):
+        super().__init__(env)
+        self.state_size = env.observation_space.shape[0]
+        self.gamma = gamma
 
-agent = Agent(env)
-state = env.reset()
-for i in range(200):
-    action = agent.get_action(state)
-    state, reward, done, info=env.step(action)
-    env.render()
+        self.build_model()
+
+    def build_model(self):
+        self.weight = np.random.random((self.state_size, self.action_size))
+        self.bias = np.random.random((1, self.action_size))*1e-2
+        self.best_weight = self.weight.copy()
+        self.best_reward = -np.Inf
+
+    def get_action(self, state):
+        result = np.dot(state, self.weight)+self.bias
+        action = np.argmax(result)
+        return action
+    
+    def update(self, reward):
+        if reward >= self.best_reward:
+            self.best_weight = self.weight.copy()
+            self.best_reward = reward
+            self.gamma = min(self.gamma/2, 1e-3)
+        else:
+            self.gamma = max(self.gamma*2, 2)
+
+        self.bias =  np.random.random((1, self.action_size))*self.gamma
+        self.weight = self.weight + self.bias
+
+
+agent = PoleAgent(env)
+for ep in range(500):
+    state = env.reset()
+    total_reward = 0
+    for i in range(200):
+        action = agent.get_action(state)
+        state, reward, done, info=env.step(action)
+        agent.update(reward)
+        env.render()
+        total_reward += reward
+    print(f"episode : {ep}, total_reward : {total_reward}")
--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-import tensorflow as tf
-state_in = tf.Tensor(1, dtype = tf.int32)
-print(state_in)
+import numpy as np
+
+a = np.array([1, 2, 3, 4])
+b = a.copy()
+
+print(type(b))
